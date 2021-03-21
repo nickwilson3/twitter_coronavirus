1af9c33071d7924bbd163a33cc3aaa08ea4fd1e9
Nick Wilson <nicholas.wilson.23@cmc.edu>
2021-03-21 15:55:29 -0700

    ready to run

--- src/map.py
+++ src/map.py
@@ -37,7 +37,7 @@ hashtags = [
 
 # initialize counters
 counter_lang = defaultdict(lambda: Counter())
-
+counter_country = defaultdict(lambda: Counter())
 # open the zipfile
 with zipfile.ZipFile(args.input_path) as archive:
 
@@ -60,9 +60,15 @@ with zipfile.ZipFile(args.input_path) as archive:
                 # search hashtags
                 for hashtag in hashtags:
                     lang = tweet['lang']
+                    try:
+                        country = tweet['place']['country_code']
+                    except Exception as e: 
+                        country = "N/A"
                     if hashtag in text:
                         counter_lang[hashtag][lang] += 1
+                        counter_country[hashtag][country] += 1
                     counter_lang['_all'][lang] += 1
+                    counter_country['all'][country] += 1
 
 # open the outputfile
 try:
@@ -72,7 +78,11 @@ except FileExistsError:
 output_path_base = os.path.join(args.output_folder,os.path.basename(args.input_path))
 
 output_path_lang = output_path_base+'.lang'
+output_path_country = output_path_base+'.country'
 print('saving',output_path_lang)
+print('saving',output_path_country)
 with open(output_path_lang,'w') as f:
     f.write(json.dumps(counter_lang))
+with open(output_path_country,'w') as f:
+    f.write(json.dumps(counter_country))
 
